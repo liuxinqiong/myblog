59c47993916d4933c9089226a0001bd577e7ecbe
liuxinqiong <372513646@qq.com>
2023-10-24 16:32:40 +0800

    add house api

---
+++ api/house.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var router = express.Router()
+var axios = require('axios')
+var CODE = require('../constant')
+
+router.get('/floor-info', function (req, res, next) {
+  var code = req.query.code
+  var url = 'http://222.240.149.21:8081'
+  var formData = new FormData()
+  formData.set('ywzh', code)
+  axios
+    .post(url + '/hslist', formData)
+    .then(function (data) {
+      res.json({
+        code: CODE.OK,
+        data: data.data,
+      })
+    })
+    .catch(next)
+})
+
+module.exports = router
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 module.exports = function (app) {
-    app.use('/api/common', require('./common'));
-    app.use('/api/todo', require('./todo'));
-    app.use('/api/link', require('./link'));
-    app.use('/api/posts', require('./posts'));
-    app.use('/api/wechat', require('./wechat'))
-};
+  app.use('/api/common', require('./common'))
+  app.use('/api/todo', require('./todo'))
+  app.use('/api/link', require('./link'))
+  app.use('/api/posts', require('./posts'))
+  app.use('/api/wechat', require('./wechat'))
+  app.use('/api/house', require('./house'))
+}
