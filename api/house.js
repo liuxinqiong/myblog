9b888ee03f8c3a6e98823160255f3811eb474817
liuxinqiong <372513646@qq.com>
2023-10-24 17:06:14 +0800

    fix house api

--- api/house.js
+++ api/house.js
@@ -1,16 +1,13 @@
 var express = require('express')
 var router = express.Router()
 var axios = require('axios')
-var FormData = require('form-data')
 var CODE = require('../constant')
 
 router.get('/floor-info', function (req, res, next) {
   var code = req.query.code
   var url = 'http://222.240.149.21:8081'
-  var formData = new FormData()
-  formData.append('ywzh', code)
   axios
-    .post(url + '/hslist', formData)
+    .post(url + '/hslist', { ywzh: code })
     .then(function (data) {
       res.json({
         code: CODE.OK,
